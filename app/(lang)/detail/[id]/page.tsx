191b07980808e944531d2df6df568bbd660d0ab9
Gaurav Kumar <gk989kumar@gmail.com>
2024-04-06 12:35:41 +0530

    dsva

---
+++ app/(lang)/detail/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const DetailPage = () => {
+  return (
+    <div>DetailPage</div>
+  )
+}
+
+export default DetailPage
---
+++ app/components/Nav.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useState } from "react";
+
+const NavItemns = [
+  { value: "P&I Online", key: "P&IOnline" },
+  { value: "UTOM", key: "utom" },
+  { value: "Azure Portal", key: "azurePortal" },
+  { value: "Product 4", key: "Product4" },
+  { value: "Product 5", key: "Product5" },
+];
+
+const DESC = {
+  "P&IOnline": [
+    {
+      text: "DWTEST",
+      link: "https://pandionlinedwtest.shipownersclub.com/",
+    },
+    {
+      text: "PREPROD",
+      link: "https://pandionlinedwtest.shipownersclub.com/",
+    },
+    {
+      text: "PERFTEST",
+      link: "https://pandionlineperf.shipownersclub.com",
+    },
+    {
+      text: "PITEST",
+      link: "https://pandionlinepitest.shipownersclub.com",
+    },
+    {
+      text: "DWDEV",
+      link: "https://pandionlinedwdev.shipownersclub.com",
+    },
+    {
+      text: "PIDEV",
+      link: "https://pandionlinedev.shipownersclub.com",
+    },
+    {
+      text: "PRODTEST",
+      link: "https://pandionlineprodtest.shipownersclub.com",
+    },
+  ],
+  azurePortal: [
+    {
+      text: "PERFTEST",
+      link: "https://pandionlineperf.shipownersclub.com",
+    },
+    {
+      text: "PITEST",
+      link: "https://pandionlinepitest.shipownersclub.com",
+    },
+    {
+      text: "DWDEV",
+      link: "https://pandionlinedwdev.shipownersclub.com",
+    },
+  ],
+};
+const Nav = () => {
+  const [selectMenu, setSelectedMenu] = useState("P&IOnline");
+  const [selectName, setName] = useState("P&I Online");
+  const [data, setData] = useState(DESC["P&IOnline"]);
+  const NavLinks = ({ value, key }) => {
+    const handleClick = () => {
+      setSelectedMenu(key);
+      setName(value);
+    };
+    return (
+      <div key={key} onClick={handleClick} className="hover:cursor-pointer">
+        {value}
+      </div>
+    );
+  };
+  const listItems = NavItemns.map(NavLinks);
+
+  return (
+    <>
+      <div className="flex justify-around  p-[10px] bg-blue-500 p-[20px] text-white">
+        {listItems}
+      </div>
+      <div>
+        <ShowInfo data={DESC[selectMenu] || []} name={selectName} />
+      </div>
+    </>
+  );
+};
+
+const ShowInfo = ({ data = [],name }) => {
+
+    const PageLinks = ({ text,link }) => {
+      
+        return (
+            <a href={"/detail/idaa"}> <div key={text}  className="hover:cursor-pointer p-[7px]  m-[5px]">
+            {text}
+          </div>
+          </a>
+        );
+      };
+      const listItems = data.map(PageLinks);
+  return <div className="w-[80%] mx-auto my-[20px]  items-center justify-center">
+   <div className="text-3xl">{name}</div>
+    {listItems}
+    
+  </div>;
+};
+
+export default Nav;
