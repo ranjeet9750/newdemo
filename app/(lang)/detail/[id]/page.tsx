7f5df65275673d6de58628f784e936bd7b09a9da
Gaurav Kumar <gk989kumar@gmail.com>
2024-04-06 18:40:51 +0530

    tables styles up[dated

--- app/(lang)/detail/[id]/page.tsx
+++ app/(lang)/detail/[id]/page.tsx
@@ -1,26 +1,15 @@
 import { Details } from "@/app/config/company"
 
 const CLASS_MAP = {
-    title: "product-title ml-3 text-center md:text-start",
-    contentImageContainer: "flex flex-col lg:flex-row justify-evenly gap-12 ",
-    contentContainer: "mt-12 flex flex-col gap-12",
-    headingContainer: "flex gap-20",
-    symbolAndTextContainer: "flex gap-6",
-    vegetarian: "h-5 w-5 bg-[#1D4E22]",
-    nonVegetarian: "h-5 w-5 bg-[#A80000]",
-    productIsFocus: "font-extrabold",
-    tableContainer: "table-fixed",
-    tableBorder: "border-b-2 ",
-    tableHeadFirstText: "p-2 pr-12 ",
-    tableHeadText: "p-2",
-    tableData: "pb-3.5 pt-7 px-12 ",
+    
+   
   }
 
 const DetailPage = ({ params }) => {
     console.log(params.id)
     console.log(Details[params.id])
   return (
-    <div>
+    <div className="w-[50%] mx-auto">
         <Table data={Details[params.id]}/>
     </div>
   )
@@ -43,7 +32,7 @@ const Table = ({ data=[] }) => {
   
     const tabLeRow = sku.map(TableRow);
     return (
-      <table className='catalogue-table'>
+      <table className='catalogue-table '>
         <thead style={{ borderRadius: '10px' }}>
               <tr>
               {data["header"].map((x)=>{
